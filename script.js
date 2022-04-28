cceb899f4dc19ed21248bd2b3e5db12863198de8
Duy Pham <dvpham89@yahoo.com>
2022-04-28 09:21:52 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const snacksGC = [
+  {
+    Category: "Desserts",
+    Name: "Funnel Cake",
+    Description: "Ducimus dignissimos ratione sed voluptates tempore.",
+    Price: 4.5,
+  },
+  {
+    Category: "Desserts",
+    Name: "Elephant Ears",
+    Description: "Facilis dolorem earum, deleniti blanditiis provident minima",
+    Price: 4.5,
+  },
+  {
+    Category: "Desserts",
+    Name: "Cotton Candy",
+    Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    Price: 3.5,
+  },
+  {
+    Category: "Snacks",
+    Name: "Popcorn",
+    Description: "Ducimus dignissimos ratione sed voluptates tempore.",
+    Price: 2.0,
+  },
+  {
+    Category: "Snacks",
+    Name: "Pretzel",
+    Description: "Maiores quisquam fugiat voluptatibus sunt exercitationem",
+    Price: 3.0,
+  },
+  {
+    Category: "Snacks",
+    Name: "French Fries",
+    Description:
+      "est deserunt necessitatibus possimus recusandae numquam consequatur animi ipsum.",
+    Price: 2.5,
+  },
+  {
+    Category: "Snacks",
+    Name: "Nachos",
+    Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    Price: 4.5,
+  },
+  {
+    Category: "Snacks",
+    Name: "Hot Dog",
+    Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    Price: 3.5,
+  },
+  {
+    Category: "Beverages",
+    Name: "Soda-Pop",
+    Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    Price: 2.0,
+  },
+  {
+    Category: "Beverages",
+    Name: "Lemonade",
+    Description: "Ducimus dignissimos ratione sed voluptates tempore.",
+    Price: 2.5,
+  },
+];
