116254c3a90d76ca4ea917346801df4ae82de9f9
Duy Pham <dvpham89@yahoo.com>
2022-05-02 11:37:07 -0400

    more-css

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ const foodItems = document.querySelector(".food-items");
 const subTotalP = document.createElement("p");
 const salesTaxP = document.createElement("p");
 const totalP = document.createElement("p");
+const checkoutDiv = document.createElement("div");
 const checkoutButton = document.createElement("button");
+const cashForm = document.querySelector(".cash-form");
+const creditForm = document.querySelector(".credit-form");
+const cashButton = document.createElement("button");
+const creditButton = document.createElement("button");
 const printReceiptButton = document.createElement("button");
 printReceiptButton.textContent = "Print Receipt";
 printReceiptButton.classList.add("print-receipt");
@@ -20,35 +25,38 @@ const snacksGC = [
     image: "assets/01-funnel-cakes.jpg",
     category: "desserts",
     name: "Funnel Cakes",
-    description: "Ducimus dignissimos ratione sed voluptates tempore.",
+    description:
+      "Cake batter deep fried to perfection, and dusted with powdered sugar.",
     price: 4.5,
   },
   {
     image: "assets/02-elephant-ears.jpg",
     category: "desserts",
     name: "Elephant Ears",
-    description: "Facilis dolorem earum, deleniti blanditiis provident minima",
+    description: "Crispy fried bread coated with cinnamon and sugar.",
     price: 4.5,
   },
   {
     image: "assets/03-cotton-candy.jpg",
     category: "desserts",
     name: "Cotton Candy",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description: "Light, fluffy, and incredibly sticky spun sugar candy.",
     price: 3.5,
   },
   {
     image: "assets/04-popcorn.jpg",
     category: "snacks",
     name: "Popcorn",
-    description: "Ducimus dignissimos ratione sed voluptates tempore.",
+    description:
+      "Kernels of corn heated and popped to fluffy perfection, and coated in butter.",
     price: 2.0,
   },
   {
     image: "assets/05-pretzels.jpg",
     category: "snacks",
     name: "Pretzel",
-    description: "Maiores quisquam fugiat voluptatibus sunt exercitationem",
+    description:
+      "Soft, doughy baked bread knotted and sprinkled with salt. Sides of mustard or cheese available on request.",
     price: 3.0,
   },
   {
@@ -56,35 +64,37 @@ const snacksGC = [
     category: "snacks",
     name: "French Fries",
     description:
-      "est deserunt necessitatibus possimus recusandae numquam consequatur animi ipsum.",
+      "Deep fried potato strips. These fries are not of French origin, nor is ketchup they come with.",
     price: 2.5,
   },
   {
     image: "assets/07-nachos.jpg",
     category: "snacks",
     name: "Nachos",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description: "Tortilla chips with warm cheese sauce.",
     price: 4.5,
   },
   {
     image: "assets/08-hot-dogs.jpg",
     category: "snacks",
     name: "Hot Dog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description: "Cured steamed sausage served with bread made just for them.",
     price: 3.5,
   },
   {
     image: "assets/09-fountain-drink.jpg",
     category: "beverages",
     name: "Fountain Drink",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "This beverage is comprised of various soda flavored syrups mixed with carbonated water and dash of sweetener. Fountain not included",
     price: 2.0,
   },
   {
     image: "assets/10-lemonade.jpg",
     category: "beverages",
     name: "Lemonade",
-    description: "Ducimus dignissimos ratione sed voluptates tempore.",
+    description:
+      "Our lemonade is made by mixing lemon flavored syrup with water and adding one pound of sugar per ounce.",
     price: 2.5,
   },
 ];
@@ -102,7 +112,7 @@ const createMenu = () => {
     image.classList.add("food-images");
     title.textContent = snack.name;
     description.textContent = snack.description;
-    price.textContent = snack.price;
+    price.textContent = `$${snack.price.toFixed(2)}`;
     addToCart.textContent = "Add to cart";
     menuLi.classList.add(snack.category);
     addToCart.classList.add("add-to-cart");
@@ -147,9 +157,6 @@ main.addEventListener("click", (e) => {
   }
 
   if (e.target.classList.contains("checkout-button")) {
-    const checkoutDiv = document.createElement("div");
-    const cashButton = document.createElement("button");
-    const creditButton = document.createElement("button");
     subTotalP.textContent = `Subtotal: $${subTotal.toFixed(2)}`;
     salesTaxP.textContent = `Sales tax: 6%`;
     total = subTotal + subTotal * 0.06;
@@ -165,8 +172,8 @@ main.addEventListener("click", (e) => {
   }
 
   if (e.target.classList.contains("cash-button")) {
-    const cashForm = document.querySelector(".cash-form");
-    cashForm.style.display = "block";
+    creditForm.style.display = "none";
+    cashForm.style.display = "flex";
     main.addEventListener("click", (e) => {
       if (e.target.classList.contains("cash-out")) {
         e.preventDefault();
@@ -188,8 +195,8 @@ main.addEventListener("click", (e) => {
 
   if (e.target.classList.contains("credit-button")) {
     e.preventDefault();
-    const creditForm = document.querySelector(".credit-form");
-    creditForm.style.display = "block";
+    cashForm.style.display = "none";
+    creditForm.style.display = "flex";
     cartUl.append(creditForm);
     main.addEventListener("click", (e) => {
       if (e.target.classList.contains("run-card")) {
@@ -208,7 +215,9 @@ main.addEventListener("click", (e) => {
     itemDiv.classList.add("item-div");
     shoppingCart.forEach((item) => {
       const itemNamePrice = document.createElement("p");
-      itemNamePrice.textContent = `Item: ${item.name} Price: $${item.price}`;
+      itemNamePrice.textContent = `Item: ${
+        item.name
+      } Price: $${item.price.toFixed(2)}`;
       subTotalP.textContent = `Subtotal: $${subTotal.toFixed(2)}`;
       salesTaxP.textContent = `Sales tax: 6%`;
       totalP.textContent = `Total: $${total.toFixed(2)}`;
